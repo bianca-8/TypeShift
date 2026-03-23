33c9e205bba4bb94285ee1b23d3cfc767795d4c8
Bianca <130195861+bianca-8@users.noreply.github.com>
2026-03-22 20:28:39 -0400

    add shift files

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
 const inputText = document.getElementById('input-text');
 const outputDiv = document.getElementById('output-text');
+const shiftSelect = document.getElementById('shift-select');
 
 let userInput = '';
+let shiftValue = 0;
 
 outputDiv.textContent = 'Encrypted text will appear here...';
 outputDiv.style.color = 'rgb(117,117,117)';
 
+shiftSelect.addEventListener('change', function() {
+    shiftValue = parseInt(shiftSelect.value);
+    updateOutput();
+});
+
 inputText.addEventListener('input', function() {
     userInput = inputText.value;
-    
+    updateOutput();
+});
+
+function updateOutput() {
     if (userInput === '') {
         outputDiv.textContent = 'Encrypted text will appear here...';
         outputDiv.style.color = 'rgb(117,117,117)';
-    } 
-    else {
+    } else {
         outputDiv.textContent = userInput;
         outputDiv.style.color = 'rgb(0,0,0)';
     }
-});
+}
